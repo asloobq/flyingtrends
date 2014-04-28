aeed83070a7f8c5d89a9d496f94f89ad9b0cb4ab
Asloob Qureshi <asloobq@gmail.com>
2014-04-28 18:55:38 -0400

    A little clean up

--- src/home.js
+++ src/home.js
@@ -26,92 +26,75 @@
 
 var HomeLayer = cc.Layer.extend({
     isMouseDown:false,
-    helloImg:null,
-    helloLabel:null,
-    circle:null,
-    sprite:null,
-    startButton:null,
-    optionsButton:null,
-    creditsButton:null,
+    mStartButton:null,
+    mOptionsButton:null,
+    mCreditsButton:null,
 	mBgImage:null,
 	mLogoImage:null,
+
+
     init:function () {
         //////////////////////////////
         // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask director the window size
         var size = cc.Director.getInstance().getWinSize();
-
-        // add a "close" icon to exit the progress. it's an autorelease object
-        /*var closeItem = cc.MenuItemImage.create(
-            "res/CloseNormal.png",
-            "res/CloseSelected.png",
-            function () {
-                history.go(-1);
-            },this);
-        closeItem.setAnchorPoint(0.5, 0.5);
-
-        var menu = cc.Menu.create(closeItem);
-        menu.setPosition(0,0);
-        this.addChild(menu, 1);
-        closeItem.setPosition(size.width - 20, 20);*/
+    
+        //Set background image
 		this.mBgImage = cc.Sprite.create(s_Clouds);
 		this.mBgImage.setPosition(size.width/2, size.height/2);
 		this.addChild(this.mBgImage);
 		
+        //Set Game title
 		this.mLogoImage = cc.Sprite.create(s_Logo);
 		this.mLogoImage.setPosition(size.width/2 + 20, size.height-80);
 		this.addChild(this.mLogoImage);
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        //this.helloLabel = cc.LabelTTF.create("FLYING TRENDS", "Arial", 38);
-        // position the label on the center of the screen
-        //this.helloLabel.setPosition(size.width / 2, size.height/2 + 100);
-        // add the label as a child to this layer
-        //this.addChild(this.helloLabel, 5);
 
-        var lazyLayer = cc.Layer.create();
-        this.addChild(lazyLayer);
+        //Add start button
+	    this.mStartButton = cc.MenuItemSprite.create(cc.Sprite.create(s_StartButton));
+	    
+	    this.mStartButton.setCallback(this.menuStartCallback, this);
+	    
+        //Add options button
+        this.mOptionsButton = cc.MenuItemSprite.create(cc.Sprite.create(s_OptionsButton));
 
-       
-
-	this.startButton = cc.MenuItemSprite.create(cc.Sprite.create("res/start_button.png"));
-	//this.startButton.setTarget(this, this.menuStartCallback);
-	this.startButton.setCallback(this.menuStartCallback, this);
-	this.optionsButton = cc.MenuItemSprite.create(cc.Sprite.create("res/options_button.png"));
-	this.creditsButton = cc.MenuItemSprite.create(cc.Sprite.create("res/credits_button.png"));
-	var menu = cc.Menu.create(this.startButton);
-	menu.addChild(this.optionsButton);
-	menu.addChild(this.creditsButton);
-	menu.setPosition(size.width / 2, size.height/2);
-	menu.alignItemsVerticallyWithPadding(50);
-	this.addChild(menu, 1);
-    //optionsButton:null,
-    //creditsButton:null
+        //Add credits button
+	    this.mCreditsButton = cc.MenuItemSprite.create(cc.Sprite.create(s_CreditsButton));
+	    
+        //Put the buttons in a menu
+        var menu = cc.Menu.create(this.mStartButton);
+	    menu.addChild(this.mOptionsButton);
+	    menu.addChild(this.mCreditsButton);
+	    menu.setPosition(size.width / 2, size.height/2);
+	    menu.alignItemsVerticallyWithPadding(20);
+	    this.addChild(menu, 1);
 
         this.setTouchEnabled(true);
 		this.setKeyboardEnabled(true);
         return true;
     },
+
+
     //start button callback
     menuStartCallback:function (sender) {
 	console.log("start clicked");
-	cc.Director.getInstance().replaceScene(new HelloWorldScene);
+        //Start Game Scene
+	    cc.Director.getInstance().replaceScene(new HelloWorldScene);
     },
+
+
     // a selector callback
     menuCloseCallback:function (sender) {
         cc.Director.getInstance().end();
     },
+
+
     onTouchesBegan:function (touches, event) {
         this.isMouseDown = true;
 	//this.heli.setPositionY(this.heli.getPositionY() + (this.speed * 20));
     },
+
+
     onTouchesMoved:function (touches, event) {
         if (this.isMouseDown) {
             if (touches) {
@@ -119,12 +102,18 @@ var HomeLayer = cc.Layer.extend({
             }
         }
     },
+
+
     onTouchesEnded:function (touches, event) {
         this.isMouseDown = false;
     },
+
+
     onTouchesCancelled:function (touches, event) {
         console.log("onTouchesCancelled");
     },
+
+
 	 handleTouch:function(touchLocation)
     {
 		//console.log("handleTouch x = ", touchLocation.x);
--- src/resource.js
+++ src/resource.js
@@ -9,20 +9,25 @@ var s_Pipe1 = "res/pipe1.png";
 var s_Pipe2 = "res/pipe2.png";
 var s_Restart = "res/restart_button.png";
 var s_Logo = "res/logo.png";
-var s_Gameover = "res/gameover.png"
+var s_Gameover = "res/gameover.png";
+var s_StartButton = "res/start_button.png"
+var s_OptionsButton = "res/options_button.png";
+var s_CreditsButton = "res/credits_button.png"; 
 var g_resources = [
     //image
     s_HelloWorld,
     s_CloseNormal,
     s_CloseSelected,
-    s_Music_Explode,
-	s_Clouds,
+    s_Clouds,
 	s_Pause,
 	s_Heli1,
 	s_Pipe1,
 	s_Pipe2,
 	s_Restart,
 	s_Logo,
+    s_StartButton,
+    s_OptionsButton,
+    s_CreditsButton,
     //plist
 
     //fnt
@@ -32,4 +37,7 @@ var g_resources = [
     //bgm
 
     //effect
+    s_Music_Explode,
+
 ];
+
